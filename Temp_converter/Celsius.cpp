d813c258a1bf00e451da6ba713eca20bd2104e8e
TuukkaUimarihuhta <121591349+TuukkaUimarihuhta@users.noreply.github.com>
2023-01-17 11:09:47 +0200

    Update Celsius.cpp

--- Temp_converter/Celsius.cpp
+++ Temp_converter/Celsius.cpp
@@ -6,6 +6,7 @@ double celsius()
 	std::cout << "Give celsius degree that you want to convert: ";
 	double C{ 0 };
 	std::cin >> C;
+	// Formula used to convert celsius to fahrenheit
 	double F = (C * 1.8) + 32;
 	return F;
 }
